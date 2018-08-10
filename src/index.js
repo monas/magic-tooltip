f4cafd4f7c24f15ba3028afa107c9705c489bd75
Laimonas Narbutas <info@monas.lt>
2018-08-10 07:39:15 +0000

    Small changes

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import MagicTooltipStore from './components/MagicTooltipStore'
+import MagicTooltip from './components/MagicTooltip'
+import MagicTooltipDisplay from './components/MagicTooltipDisplay'
+
+export {
+    MagicTooltipStore,
+    MagicTooltip,
+    MagicTooltipDisplay
+}
